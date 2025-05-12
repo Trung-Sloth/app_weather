e0027f9ea6ddb23b8d0892ceb81933a98b9901d9
trungdz <tranquoctrung6a8@gmail.com>
2025-05-13 03:43:48 +0700

    #

--- weatherProject/forecast/static/js/chartSetup.js
+++ weatherProject/forecast/static/js/chartSetup.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded',() => {
-    const chartElement=document.getElementById('chart');
+document.addEventListener('DOMContentLoaded', () => {
+    const chartElement = document.getElementById('chart');
     if (!chartElement) {
         console.error('Canvas Element nt found.');
         return;
     }
 
-    const ctx=chartElement.getContext('2d');
-    const gradient = ctx.createLinearGradient(0, -10 , 0, 100);
+    const ctx = chartElement.getContext('2d');
+    const gradient = ctx.createLinearGradient(0, -10, 0, 100);
     gradient.addColorStop(0, 'rgba(250, 0, 0, 1)');
     gradient.addColorStop(1, 'rgba(136, 255, 0, 1');
 
     const forecastItems = document.querySelectorAll('.forecast-item');
 
-    const temps =[];
-    const times =[];
+    const temps = [];
+    const times = [];
 
     forecastItems.forEach(item => {
-        const time=item.querySelector('.forecast-time').textContent;
-        const temp=item.querySelector('.forecast-temperatureValue').textContent;
-        const hum=item.querySelector('.forecast-humidityValue').textContent;
+        const time = item.querySelector('.forecast-time').textContent;
+        const temp = item.querySelector('.forecast-temperatureValue').textContent;
+        const hum = item.querySelector('.forecast-humidityValue').textContent;
 
         if (time && temp && hum) {
             times.push(time);
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded',() => {
         }
     });
 
-    if (temps.length === 0 || times.length ===0) {
+    if (temps.length === 0 || times.length === 0) {
         console.error('Temp or time values are missing.');
         return;
     }
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded',() => {
                 },
             },
             scales: {
-                x : {
+                x: {
                     display: false,
                     grid: {
                         drawOnChartArea: false,
--- weatherProject/forecast/static/js/main.js
+++ weatherProject/forecast/static/js/main.js
@@ -1,190 +1,215 @@
-document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener('DOMContentLoaded', () => {
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyAmd8V46CLS11cyu1UnjqBwtcBUXybnyNA",
-        authDomain: "map-1-b0eae.firebaseapp.com",
-        databaseURL: "https://map-1-b0eae-default-rtdb.asia-southeast1.firebasedatabase.app/",
-        projectId: "map-1-b0eae",
-        storageBucket: "map-1-b0eae.appspot.com",
-    };
-    
-    firebase.initializeApp(firebaseConfig);
-    const db = firebase.database();
-    const map = L.map('map').setView([10.850324, 106.772186], 20);
-    
-    //   L.tileLayer('https://api.maptiler.com/maps/openstreetmap/256/{z}/{x}/{y}.jpg?key=WxV6QKoeVDywcxuiW3su', // Map bth
-    //   { 
-    //     tileSize: 256,
-    //     zoomOffset: 0,
-    //     maxZoom: 22
-    //   }).addTo(map);
+  const firebaseConfig = {
+    apiKey: "AIzaSyAmd8V46CLS11cyu1UnjqBwtcBUXybnyNA",
+    authDomain: "map-1-b0eae.firebaseapp.com",
+    databaseURL: "https://map-1-b0eae-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "map-1-b0eae",
+    storageBucket: "map-1-b0eae.appspot.com",
+  };
 
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', //Map ve tinh
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
+  const map = L.map('map').setView([10.850324, 106.772186], 20);
+
+  //   L.tileLayer('https://api.maptiler.com/maps/openstreetmap/256/{z}/{x}/{y}.jpg?key=WxV6QKoeVDywcxuiW3su', // Map bth
+  //   { 
+  //     tileSize: 256,
+  //     zoomOffset: 0,
+  //     maxZoom: 22
+  //   }).addTo(map);
+
+  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', //Map ve tinh
     {
-        maxZoom: 19
+      maxZoom: 19
     }).addTo(map);
-    
-    // let currentMarker = null;
-    let currentMarker1 = null;
-    let currentMarker2 = null;
-    let currentMarker3 = null;
-    var n=0;
-    var x1 = 0;
-    var x2 = 0;
-    var x3 = 0;
-    var y1 = 0;
-    var y2 = 0;
-    var y3 = 0;
-    
-    // map.on('click', function(e) {
-    //     const lat = e.latlng.lat.toFixed(6);
-    //     const lng = e.latlng.lng.toFixed(6);
-    
-    //     db.ref("Toa-do").set({
-    //     lat: parseFloat(lat),
-    //     lng: parseFloat(lng),
-    //     })
-    //     .then(() => {
-    //     document.getElementById('status').innerText = `Vi-do=${lat}, Kinh-do=${lng}`;
-    //     })
-    //     .catch((error) => {
-    //     document.getElementById('status').innerText = `Error: ${error}`;
-    //     });
-    
-    
-    //     if (currentMarker) {
-    //     map.removeLayer(currentMarker);
-    //     }
-    
-    //     currentMarker = L.marker([lat, lng]).addTo(map);
-    // });
 
-    map.on('click', function(e) {
-        n=n+1;
-        if(n%3==1)
-        {
-          const lat1 = e.latlng.lat.toFixed(6);
-          const lng1 = e.latlng.lng.toFixed(6);
-          x1 = lat1;
-          y1 = lng1;
-          
-          
-          // db.ref("Toa-do-1").set({
-          //   lat: parseFloat(lat1),
-          //   lng: parseFloat(lng1),
-          // })
-      
-          if ((n%3==1) && (n>3)) {
-            map.removeLayer(currentMarker1);
-          }
-          currentMarker1 = L.marker([lat1, lng1]).addTo(map);
-        }
-        
-        if(n%3==2)
-          {
-            const lat2 = e.latlng.lat.toFixed(6);
-            const lng2 = e.latlng.lng.toFixed(6);
-            x2 = lat2;
-            y2 = lng2;
-            
-            // db.ref("Toa-do-2").set({
-            //   lat: parseFloat(lat2),
-            //   lng: parseFloat(lng2),
-            // })
-            
-            if ((n%3==2) && (n>3)) {
-              map.removeLayer(currentMarker2);
-            }
-            currentMarker2 = L.marker([lat2, lng2]).addTo(map);
-          }
-        
-          if(n%3==0)
-            {
-              const lat3 = e.latlng.lat.toFixed(6);
-              const lng3 = e.latlng.lng.toFixed(6);
-              x3 = lat3;
-              y3 = lng3;
-              
-              // db.ref("Toa-do-3").set({
-              //   lat: parseFloat(lat3),
-              //   lng: parseFloat(lng3),
-              // })
-      
-              if ((n%3==0) && (n>3)) {
-                map.removeLayer(currentMarker3);
-              }
-              currentMarker3 = L.marker([lat3, lng3]).addTo(map);
-            }
-      });
-    
-    const toggleBtn = document.getElementById('toggleBtn');
-    const mapDiv = document.getElementById('map');
-    const confirmBtn = document.getElementById('confirmBtn');
-    const deleteBtn = document.getElementById('deleteBtn');
-    
-    toggleBtn.addEventListener('click', () => {
-      if(confirmBtn.style.display == 'none')
-      {
-        confirmBtn.style.display = 'block';
-      }
-      else  confirmBtn.style.display = 'none';
+  const redIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
 
-      if(deleteBtn.style.display == 'none')
-      {
-        deleteBtn.style.display = 'block';
-      }
-      else  deleteBtn.style.display = 'none';
-      
-        if (mapDiv.style.display === 'none') {
-            mapDiv.style.display = 'block';
-            toggleBtn.innerText = 'Hide map';
-            map.invalidateSize();
-        } 
-        else {
-            mapDiv.style.display = 'none';
-            // document.getElementById('status').innerText = '';
-            document.getElementById('status1').innerText = '';
-            document.getElementById('status2').innerText = '';
-            document.getElementById('status3').innerText = '';
-            toggleBtn.innerText = 'Show map';
-        }
-    });
+  const greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
 
-    confirmBtn.addEventListener('click', () => {
-      if(n!=0)
-      {
-        document.getElementById('status1').innerText = `Vi-do1=${x1}, Kinh-do1=${y1}`;
-        document.getElementById('status2').innerText = `Vi-do2=${x2}, Kinh-do2=${y2}`;
-        document.getElementById('status3').innerText = `Vi-do3=${x3}, Kinh-do3=${y3}`;
-      }
-      else{
-        document.getElementById('status1').innerText = '';
-        document.getElementById('status2').innerText = '';
-        document.getElementById('status3').innerText = '';
-      }
-        
-        db.ref("Toa-do-1").set({
-            lat: parseFloat(x1),
-            lng: parseFloat(y1),
-          })
-        db.ref("Toa-do-2").set({
-            lat: parseFloat(x2),
-            lng: parseFloat(y2),
-          })
-        db.ref("Toa-do-3").set({
-            lat: parseFloat(x3),
-            lng: parseFloat(y3),
-          })
-    });
-      
-    deleteBtn.addEventListener('click', () => {
+  const blueIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+  // let currentMarker = null;
+  let currentMarker1 = null;
+  let currentMarker2 = null;
+  let currentMarker3 = null;
+  var n = 0;
+  var x1 = 0, x2 = 0, x3 = 0;
+  var y1 = 0, y2 = 0, y3 = 0;
+
+  // map.on('click', function(e) {
+  //     const lat = e.latlng.lat.toFixed(6);
+  //     const lng = e.latlng.lng.toFixed(6);
+
+  //     db.ref("Toa-do").set({
+  //     lat: parseFloat(lat),
+  //     lng: parseFloat(lng),
+  //     })
+  //     .then(() => {
+  //     document.getElementById('status').innerText = `Vi-do=${lat}, Kinh-do=${lng}`;
+  //     })
+  //     .catch((error) => {
+  //     document.getElementById('status').innerText = `Error: ${error}`;
+  //     });
+
+
+  //     if (currentMarker) {
+  //     map.removeLayer(currentMarker);
+  //     }
+
+  //     currentMarker = L.marker([lat, lng]).addTo(map);
+  // });
+
+  map.on('click', function (e) {
+    n = n + 1;
+    if (n % 3 == 1) {
+      const lat1 = e.latlng.lat.toFixed(6);
+      const lng1 = e.latlng.lng.toFixed(6);
+      x1 = lat1;
+      y1 = lng1;
+
+
+      // db.ref("Toa-do-1").set({
+      //   lat: parseFloat(lat1),
+      //   lng: parseFloat(lng1),
+      // })
+
+      if ((n % 3 == 1) && (n > 3)) {
         map.removeLayer(currentMarker1);
+      }
+      currentMarker1 = L.marker([lat1, lng1], { icon: redIcon }).addTo(map);
+    }
+
+    if (n % 3 == 2) {
+      const lat2 = e.latlng.lat.toFixed(6);
+      const lng2 = e.latlng.lng.toFixed(6);
+      x2 = lat2;
+      y2 = lng2;
+
+      // db.ref("Toa-do-2").set({
+      //   lat: parseFloat(lat2),
+      //   lng: parseFloat(lng2),
+      // })
+
+      if ((n % 3 == 2) && (n > 3)) {
         map.removeLayer(currentMarker2);
+      }
+      currentMarker2 = L.marker([lat2, lng2], { icon: greenIcon }).addTo(map);
+    }
+
+    if (n % 3 == 0) {
+      const lat3 = e.latlng.lat.toFixed(6);
+      const lng3 = e.latlng.lng.toFixed(6);
+      x3 = lat3;
+      y3 = lng3;
+
+      // db.ref("Toa-do-3").set({
+      //   lat: parseFloat(lat3),
+      //   lng: parseFloat(lng3),
+      // })
+
+      if ((n % 3 == 0) && (n > 3)) {
         map.removeLayer(currentMarker3);
-        document.getElementById('status1').innerText = '';
-        document.getElementById('status2').innerText = '';
-        document.getElementById('status3').innerText = '';
-        n=0;
-    });
+      }
+      currentMarker3 = L.marker([lat3, lng3], { icon: blueIcon }).addTo(map);
+    }
+  });
+
+  const toggleBtn = document.getElementById('toggleBtn');
+  const mapPin = document.getElementById('map-pin');
+  const mapDiv = document.getElementById('map');
+  const confirmBtn = document.getElementById('confirmBtn');
+  const deleteBtn = document.getElementById('deleteBtn');
+
+  toggleBtn.addEventListener('click', () => {
+    if (confirmBtn.style.display == 'none') {
+      confirmBtn.style.display = 'block';
+    }
+    else confirmBtn.style.display = 'none';
+
+    if (deleteBtn.style.display == 'none') {
+      deleteBtn.style.display = 'block';
+    }
+    else deleteBtn.style.display = 'none';
+
+    if (mapDiv.style.display === 'none') {
+      mapDiv.style.display = 'block';
+      toggleBtn.innerText = 'Hide map';
+      map.invalidateSize();
+      toggleBtn.style.opacity = '0.4';
+    }
+    else {
+      mapDiv.style.display = 'none';
+      // document.getElementById('status').innerText = '';
+      document.getElementById('status1').innerText = '';
+      document.getElementById('status2').innerText = '';
+      document.getElementById('status3').innerText = '';
+      toggleBtn.innerText = 'Show map';
+      toggleBtn.style.opacity = '1.0';
+    }
+
+    if (mapPin.style.display == 'none') {
+      mapPin.style.display = 'block';
+    }
+    else mapPin.style.display = 'none';
+  });
+
+  confirmBtn.addEventListener('click', () => {
+    if (n != 0) {
+      document.getElementById('status1').innerText = `Vi-do1=${x1}, Kinh-do1=${y1}`;
+      document.getElementById('status2').innerText = `Vi-do2=${x2}, Kinh-do2=${y2}`;
+      document.getElementById('status3').innerText = `Vi-do3=${x3}, Kinh-do3=${y3}`;
+    }
+    else {
+      document.getElementById('status1').innerText = '';
+      document.getElementById('status2').innerText = '';
+      document.getElementById('status3').innerText = '';
+    }
+
+    db.ref("Toa-do-1").set({
+      lat: parseFloat(x1),
+      lng: parseFloat(y1),
+    })
+    db.ref("Toa-do-2").set({
+      lat: parseFloat(x2),
+      lng: parseFloat(y2),
+    })
+    db.ref("Toa-do-3").set({
+      lat: parseFloat(x3),
+      lng: parseFloat(y3),
+    })
+  });
+
+  deleteBtn.addEventListener('click', () => {
+    map.removeLayer(currentMarker1);
+    map.removeLayer(currentMarker2);
+    map.removeLayer(currentMarker3);
+    document.getElementById('status1').innerText = '';
+    document.getElementById('status2').innerText = '';
+    document.getElementById('status3').innerText = '';
+    n = 0;
+  });
 
 });
